2f6696c31f5fd26888b324072e41e7c1de114ea6
Rocky <rocky.amdad@gmail.com>
2016-12-21 17:58:12 +0600

    added form collection

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import {Contact} from "./contact";
 import {ContactStoreService} from "../contact-store.service";
-import {FormBuilder, FormGroup,  Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators, FormArray, FormControl} from "@angular/forms";
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -12,26 +12,47 @@ export class ContactComponent implements OnInit {
 
   form: FormGroup;
   contacts: Array<Contact>;
-  constructor(private contactService: ContactStoreService,private formBuilder: FormBuilder) {
-    this.form = formBuilder.group({
+  constructor(){}
+ /* constructor(private contactService: ContactStoreService,private formBuilder: FormBuilder) {
+    /!*this.form = formBuilder.group({
       'firstName': formBuilder.control('',Validators.required),
       'lastName': formBuilder.control('',Validators.required),
       'email': formBuilder.control('',Validators.required),
-      'phone': formBuilder.control('')
-    });
-  }
+      'phone': new FormArray()
+    });*!/
+  }*/
   //contact: Contact;
 
-  ngOnInit():any {
+  ngOnInit(){
 
+    this.form = new FormGroup({
+      'firstName': new FormControl('',[Validators.required]),
+      'lastName': new FormControl('',[Validators.required]),
+      'email': new FormControl('',[Validators.required]),
+      'phone': new FormArray([ContactComponent.getContactFormGroup()])
+    });
 
     //this.contact = new Contact('','','','');
 
-    this.loadContacts();
+    //this.loadContacts();
+  }
+  private  static getContactFormGroup(){
+    return new FormGroup({
+      'number':new FormControl('',[Validators.required]),
+      'type':new FormControl('',[Validators.required])
+    });
+  }
+  addNumbers(){
+
+    this.form.get('phone').push(ContactComponent.getContactFormGroup());
   }
   onSubmit(){
-    this.contactService.insertContact(this.form.value);
-    this.loadContacts();
+    console.log(this.form);
+   /* this.contactService.insertContact(this.form.value);
+    this.loadContacts();*/
+  }
+  removeNumber(i){
+    this.form.get('phone').removeAt(i);
   }
 
   loadContacts() {
